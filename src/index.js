ee68d10b399c9b36447b40ad66e2d9c6f4c1977c
menelik7 <menelikworku@gmail.com>
2023-07-12 13:18:11 +0200

    Repo name changed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+console.log("Repo name changed");
+
 const el = document.getElementById("root");
 const root = createRoot(el);
 
